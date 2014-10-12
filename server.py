59a554c7d2ae2b43aa00db922718b42f84c0cacd
Tobias Hansson <tobias@tobiashansson.nu>
2014-10-12 14:28:41 +0200

    Implemented very unsafe serving of web pages, but it'll do for mah mirror

--- server.py
+++ server.py
@@ -20,6 +20,7 @@ import urlparse
 import sys
 import json
 import datetime
+import os.path
 
 from apiclient.discovery import build
 from oauth2client.client import AccessTokenRefreshError
@@ -37,54 +38,35 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
                              sys.argv[2],
                              'https://www.googleapis.com/auth/calendar',
                              redirect_uri='http://mirroronthewall.se:8080/')
+  runPath = sys.argv[0]
+  runPath = runPath[:len(runPath)-9]
 
   def do_GET(self):
     """Handler for GET request."""
     print '\nNEW REQUEST, Path: %s' % (self.path)
     print 'Headers: %s' % self.headers
 
-    # To use this server, you first visit
-    # http://localhost:8080/?user=<some_user_name>. You can use any name you
-    # like for the user. It's only used as a key to store credentials,
-    # and has no relationship with real user names. In a real system, you would
-    # only use logged-in users for your system.
     if self.path.startswith('/?user='):
-      # Initial page entered by user
       self.handle_initial_url()
-
-    # When you redirect to the authorization server below, it redirects back
-    # to to http://localhost:8080/?code=<some_code> after the user grants access
-    # permission for your application.
     elif self.path.startswith('/?code='):
-      # Page redirected back from auth server
       self.handle_redirected_url()
-    # Only the two URL patterns above are accepted by this server.
     else:
-      # Either an error from auth server or bad user entered URL.
-      self.respond_ignore()
+      self.respond_with_file()
+
+  def send_template(self, calendarData):
+    """Build and send index.html template"""
+
 
   def handle_initial_url(self):
     """Handles the initial path."""
-    # The fake user name should be in the URL query parameters.
     user = self.get_user_from_url_param()
-
-    # Call a helper function defined below to get the credentials for this user.
     credentials = self.get_credentials(user)
 
-    # If there are no credentials for this fake user or they are invalid,
-    # we need to get new credentials.
     if credentials is None or credentials.invalid:
-      # Call a helper function defined below to respond to this GET request
-      # with a response that redirects the browser to the authorization server.
       self.respond_redirect_to_auth_server(user)
     else:
       try:
-        # Call a helper function defined below to get calendar data for this
-        # user.
         calendar_output = self.get_calendar_data(credentials)
-
-        # Call a helper function defined below which responds to this
-        # GET request with data from the calendar.
         self.respond_calendar_data(calendar_output)
       except AccessTokenRefreshError:
         # This may happen when access tokens expire. Redirect the browser to
@@ -93,58 +75,16 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
   def handle_redirected_url(self):
     """Handles the redirection back from the authorization server."""
-    # The server should have responded with a "code" URL query parameter. This
-    # is needed to acquire credentials.
     code = self.get_code_from_url_param()
-
-    # Before we redirected to the authorization server, we set a cookie to save
-    # the fake user for retrieval when handling the redirection back to this
-    # server. This is only needed because we are using this fake user
-    # name as a key to access credentials.
     user = self.get_user_from_cookie()
-
-    #
-    # This is an important step.
-    #
-    # We take the code provided by the authorization server and pass it to the
-    # flow.step2_exchange() function. This function contacts the authorization
-    # server and exchanges the "code" for credentials.
     credentials = RequestHandler.flow.step2_exchange(code)
-
-    # Call a helper function defined below to save these credentials.
     self.save_credentials(user, credentials)
-
-    # Call a helper function defined below to get calendar data for this user.
     calendar_output = self.get_calendar_data(credentials)
-
-    # Call a helper function defined below which responds to this GET request
-    # with data from the calendar.
     self.respond_calendar_data(calendar_output)
 
   def respond_redirect_to_auth_server(self, user):
     """Respond to the current request by redirecting to the auth server."""
-    #
-    # This is an important step.
-    #
-    # We use the flow object to get an authorization server URL that we should
-    # redirect the browser to. We also supply the function with a redirect_uri.
-    # When the auth server has finished requesting access, it redirects
-    # back to this address. Here is pseudocode describing what the auth server
-    # does:
-    #   if (user has already granted access):
-    #     Do not ask the user again.
-    #     Redirect back to redirect_uri with an authorization code.
-    #   else:
-    #     Ask the user to grant your app access to the scope and service.
-    #     if (the user accepts):
-    #       Redirect back to redirect_uri with an authorization code.
-    #     else:
-    #       Redirect back to redirect_uri with an error code.
     uri = RequestHandler.flow.step1_get_authorize_url()
-
-    # Set the necessary headers to respond with the redirect. Also set a cookie
-    # to store our user name. We will need this when the auth server
-    # redirects back to this server.
     print 'Redirecting %s to %s' % (user, uri)
     self.send_response(301)
     self.send_header('Cache-Control', 'no-cache')
@@ -152,16 +92,18 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     self.send_header('Set-Cookie', 'user=%s' % user)
     self.end_headers()
 
-  def respond_ignore(self):
-    """Responds to the current request that has an unknown path."""
-    self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
-    self.send_header('Cache-Control', 'no-cache')
-    self.end_headers()
-    self.wfile.write(
-      'This path is invalid or user denied access:\n%s\n\n' % self.path)
-    self.wfile.write(
-      'User entered URL should look like: http://mirroronthewall.se:8080/?user=johndoe')
+  def respond_with_file(self):
+    requestedFile = self.path[1:]
+    if os.path.isfile(requestedFile):
+      self.send_file(requestedFile)
+    else:
+      """Responds to the current request that has an unknown path."""
+      self.send_response(404)
+      self.send_header('Content-type', 'text/plain')
+      self.send_header('Cache-Control', 'no-cache')
+      self.end_headers()
+      self.wfile.write(
+        'This path is invalid %s\n\n' % self.path)
 
   def respond_calendar_data(self, calendar_output):
     """Responds to the current request by writing calendar data to stream."""
@@ -170,6 +112,25 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     self.send_header('Cache-Control', 'no-cache')
     self.end_headers()
     self.wfile.write(calendar_output)
+
+  def send_file(self,requestedFile):
+    self.send_response(200)
+    self.send_header('Content-type', self.resolve_content_type(requestedFile))
+    self.send_header('Cache-control', 'no-cache')
+    self.end_headers()
+    f = open(self.runPath + requestedFile, 'r')
+    #This is pretty unsafe! Never do this outside the mirror :D
+    self.wfile.write(f.read())
+
+  def resolve_content_type(self, requestedFile):
+    if requestedFile.endswith('.html'):
+      return 'text/html; charset=utf-8'
+    elif requestedFile.endswith('.css'):
+      return 'text/css; charset=utf-8'
+    elif requestedFile.endswith('.js'):
+      return 'application/javascript'
+    else:
+      return 'text/plain; charset=utf8'
 
   def get_calendar_data(self, credentials):
     """Given the credentials, returns calendar data."""
@@ -180,6 +141,33 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
     service = build('calendar', 'v3', http=http)
 
+    calendars = self.get_calendars(service)
+  
+    today = datetime.datetime.now().isoformat() + '+02:00'
+    inAWeek = (datetime.datetime.now() + datetime.timedelta(days=7)).isoformat() + '+01:00'
+    eventList = []
+    for calendar in calendars:
+      eventList = self.get_events(service, calendar, eventList, today, inAWeek)
+
+    output.write(json.dumps(eventList))
+    return output.getvalue()
+  
+  def get_events(self, service, calendar, eventList, today, inAWeek):
+    request = service.events().list(calendarId=calendar,timeMin=today,timeMax=inAWeek,singleEvents='true')
+    while request != None:
+      response = request.execute()
+      for event in response.get('items', []):
+        if event.get('status') != u'cancelled':
+          date = event.get('start').get('dateTime',event.get('start').get('date'))
+          currentEvent = {}
+          currentEvent['summary'] = event.get('summary')
+          currentEvent['date'] = date
+          eventList.append(currentEvent)
+
+      request = service.events().list_next(request, response)
+    return eventList
+  
+  def get_calendars(self, service):
     calendars = []
     request = service.calendarList().list()
     while request != None:
@@ -187,28 +175,9 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
       for calendar in response.get('items',[]):
         calendars.append(calendar.get('id',''))
       request = service.calendarList().list_next(request,response)
+
+    return calendars
   
-    today = datetime.datetime.now().isoformat() + '+02:00'
-    inAWeek = (datetime.datetime.now() + datetime.timedelta(days=7)).isoformat() + '+01:00'
-    print inAWeek
-    eventList = []
-    for calendar in calendars:
-      request = service.events().list(calendarId=calendar,timeMin=today,timeMax=inAWeek)
-      while request != None:
-        response = request.execute()
-        for event in response.get('items', []):
-          if event.get('status') != u'cancelled':
-            date = event.get('start').get('dateTime',event.get('start').get('date'))
-            currentEvent = {}
-            currentEvent['summary'] = event.get('summary')
-            currentEvent['date'] = date
-            eventList.append(currentEvent)
-      
-        request = service.events().list_next(request, response)
-
-    output.write(json.dumps(eventList))
-    return output.getvalue()
-
   def get_credentials(self, user):
     """Using the fake user name as a key, retrieve the credentials."""
     storage = Storage('credentials-%s.dat' % (user))
@@ -223,7 +192,6 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     """Get the user query parameter from the current request."""
     parsed = urlparse.urlparse(self.path)
     user = urlparse.parse_qs(parsed.query)['user'][0]
-    print 'Fake user from URL: %s' % user
     return user
 
   def get_user_from_cookie(self):
@@ -231,7 +199,6 @@ class RequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     cookies = Cookie.SimpleCookie()
     cookies.load(self.headers.get('Cookie'))
     user = cookies['fake_user'].value
-    print 'Fake user from cookie: %s' % user
     return user
 
   def get_code_from_url_param(self):
